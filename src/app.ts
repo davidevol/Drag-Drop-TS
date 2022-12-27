3c90f1e65e057d2e7ff97d4722fe99df4c368c22
De <deehzi@pm.me>
2022-12-27 18:02:05 -0300

    setting up DOM Elements

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,33 @@ class ProjectInput {
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
 
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
+
     constructor() {
         this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!;
         this.hostElement = <HTMLDivElement>document.getElementById('app')!;
 
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = <HTMLFormElement>importedNode.firstElementChild
+        this.element.id = 'user-input'
+
+        this.titleInputElement = <HTMLInputElement>this.element.querySelector('#title')
+        this.descriptionInputElement = <HTMLInputElement>this.element.querySelector('#description')
+        this.peopleInputElement = <HTMLInputElement>this.element.querySelector('#people')
+        
+        this.configure();
         this.attach();
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
     }
 
     private attach() {
