a3c0d8594745290e73e549bf71a7c727006f4267
De <deehzi@pm.me>
2022-12-29 08:09:50 -0300

    Rendering Items with a class

--- src/app.ts
+++ src/app.ts
@@ -147,6 +147,28 @@ abstract class ComponentKit<T extends HTMLElement, U extends HTMLElement> {
   abstract renderContent(): void;
 }
 
+class ProjectItem extends ComponentKit<HTMLUListElement, HTMLLIElement>{
+  private project: Project;
+
+  constructor(hostId: string, project: Project) {
+    super('single-project', hostId, false, project.id);
+    this.project = project;
+
+    this.configure();
+    this.renderContent();
+  }
+
+  configure(): void {
+      
+  }
+
+  renderContent(): void {
+      this.element.querySelector('h2')!.textContent = this.project.title;
+      this.element.querySelector('h3')!.textContent = this.project.people.toString();
+      this.element.querySelector('p')!.textContent = this.project.title;
+  }
+}
+
 class ProjectList extends ComponentKit<HTMLDivElement, HTMLElement>{
   assignedProjects: Project[];
 
@@ -183,9 +205,7 @@ class ProjectList extends ComponentKit<HTMLDivElement, HTMLElement>{
       document.getElementById(`${this.type}-projects-list`)!);
       listElement.innerHTML = "";
     for (const projectItem of this.assignedProjects) {
-      const listItem = document.createElement("li");
-      listItem.textContent = projectItem.title;
-      listElement.appendChild(listItem);
+    new ProjectItem(this.element.querySelector('ul')!.id, projectItem)
     }
   }
 }
